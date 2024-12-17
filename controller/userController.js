ecd14b2e2e1f07d5a5a3573def80b38dd591fc40
heegox <xariirburaale@outlook.com>
2024-12-17 03:07:50 +0100

    Add files via upload

---
+++ controller/userController.js
@@ -0,0 +1,38 @@
+import User from "../model/userModel.js"
+
+export const create = async (req, res)=>{
+    try{
+         const userData = new User(req.body);
+         const {email}= userData;
+
+         const userExist = await User.findOne({email})
+         if(userExist){
+            return res.status(400).json({message : "User already exists."})
+         }
+         const saveUser = await userData.save();
+         res.status(200).json(saveUser);
+    } catch (error) {
+        res.status(500).json({error:"internal server error."});
+    }
+};
+
+export const fetch = async (req, res)=>{
+    try {
+          const users = await User.find();
+          if (users.length==0){
+             return res.status(404).json({message:"user Not found."});
+          }
+          res.status(200).json(users);
+
+    } catch (error) {
+        res.status(500).json({error:"internal server error."});
+    }
+};
+
+export const update = async(req, res)=>{
+    try{
+
+    }catch (error) {
+        res.status(500).json({error:"internal server error."});
+    }
+}
